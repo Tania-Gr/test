c7c939846d2f2b2e7c85a56a44cceced813a19a6
Tania-Gr <tatartweb333@gmail.com>
2025-03-25 17:38:18 +0300

    lesson-3

---
+++ Lesson-3.1/lesson-03.js
@@ -0,0 +1,16 @@
+  // titleProject (string)
+  // creensValue (string)
+  // screenPrice (num)
+  // percentage (num)
+  // fullPrice (num)
+  //  (bool)
+
+
+let titleProject = "Калькулятор о разработке сайта"
+let creensValue = "Шаблонные, с уникальным дизайном, с анимациями"
+let screenPrice = 1000
+let percentage = 10
+let fullPrice = 10000
+let responsive = true
+
+console.log(titleProject);
